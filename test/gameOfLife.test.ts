22886c94ebd9f3bbfbe6e54ff2e40998f21bba96
klif <klif.cheloul@gmail.com>
2020-07-01 08:02:57 +0200

    all cells are dead green

--- test/gameOfLife.test.ts
+++ test/gameOfLife.test.ts
@@ -1,9 +1,19 @@
 import {GameOfLife} from "../src/GameOfLife";
 
 describe('rename me', () => {
-    it('cell should be dead when all cells are dead', () => {
+    it('unique cell should be dead when all cells are dead', () => {
       const gameOfLife = GameOfLife.of(5,5)
       gameOfLife.nextStep()
       expect(gameOfLife.cell(2,2).isAlive()).toBe(false)
+    })
+    it('all cells should stay dead when all cells are dead', () => {
+      const gameOfLife = GameOfLife.of(5,5)
+      gameOfLife.nextStep();
+      gameOfLife.allCells().forEach(cell => {
+          expect(cell.isAlive()).toBe(false)
+      })
+
+
+
     })
 })
