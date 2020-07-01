c40d4ef37d7751b6f761e5529e3e4468bdcbf6bc
klif <klif.cheloul@gmail.com>
2020-07-01 12:54:16 +0200

    trois cellules refacto

--- src/GameOfLife.ts
+++ src/GameOfLife.ts
@@ -8,17 +8,17 @@ export class GameOfLife {
     }
 
     nextStep(): boolean[][] {
-        const actualCells = this.cells
         const lines = this.cells.length
         const columns = this.cells[0].length
-        const newState = []
+        const newGOLState = []
         for (let l = 0; l < lines; l++) {
-            newState.push([])
+            newGOLState.push([])
             for (let c = 0; c < columns; c++) {
-                newState[l].push(this.setCellDependOfNeighbor(actualCells, l, c))
+                newGOLState[l].push(this.setCellDependOfNeighbor(l, c))
             }
         }
-        return newState
+        this.cells = newGOLState
+        return newGOLState
 
     }
 
@@ -27,8 +27,8 @@ export class GameOfLife {
         return this.cells
     }
 
-    private setCellDependOfNeighbor(cells: boolean[][], l: number, c:number): boolean{
-        const numberOfAliveNeighbor = this.numberOfAliveNeighborOfCell(cells, l, c)
+    private setCellDependOfNeighbor(l: number, c:number): boolean{
+        const numberOfAliveNeighbor = this.numberOfAliveNeighborOfCell(l, c)
         if (numberOfAliveNeighbor <= 1 ) {
             return false
         }
@@ -39,37 +39,37 @@ export class GameOfLife {
         return this.cells[l][c]
     }
 
-    private numberOfAliveNeighborOfCell(cells: boolean[][], l: number, c: number): number {
-        return this.neighborOfCell(cells, l, c).filter(cell => cell === true).length
+    private numberOfAliveNeighborOfCell(l: number, c: number): number {
+        return this.neighborOfCell(l, c).filter(cell => cell === true).length
     }
 
-    private neighborOfCell(cells: boolean[][], l: number, c: number): boolean[] {
-        const lines = cells.length
-        const columns = cells[0].length
+    private neighborOfCell(l: number, c: number): boolean[] {
+        const lines = this.cells.length
+        const columns = this.cells[0].length
         if (l === 0 && c === 0) {
-            return [cells[0][1], cells[1][0], cells[1][1]]
+            return [this.cells[0][1], this.cells[1][0], this.cells[1][1]]
         }
         if (l === 0 && c === columns - 1) {
-            return [cells[0][c - 1], cells[1][c], cells[1][c - 1]]
+            return [this.cells[0][c - 1], this.cells[1][c], this.cells[1][c - 1]]
         }
         if (l === lines - 1 && c === 0) {
-            return [cells[l - 1][0], cells[l][1], cells[l - 1][1]]
+            return [this.cells[l - 1][0], this.cells[l][1], this.cells[l - 1][1]]
         }
         if (l === lines - 1 && c === columns - 1) {
-            return [cells[l - 1][c], cells[l][c - 1], cells[l - 1][c - 1]]
+            return [this.cells[l - 1][c], this.cells[l][c - 1], this.cells[l - 1][c - 1]]
         }
         if (l === 0) {
-            return [cells[0][c - 1], cells[0][c + 1], cells[1][c - 1], cells[1][c], cells[1][c + 1],]
+            return [this.cells[0][c - 1], this.cells[0][c + 1], this.cells[1][c - 1], this.cells[1][c], this.cells[1][c + 1],]
         }
         if (l === lines - 1) {
-            return [cells[l][c - 1], cells[l][c + 1], cells[l - 1][c - 1], cells[l - 1][c], cells[l - 1][c + 1],]
+            return [this.cells[l][c - 1], this.cells[l][c + 1], this.cells[l - 1][c - 1], this.cells[l - 1][c], this.cells[l - 1][c + 1],]
         }
         if (c === 0) {
-            return [cells[l - 1][0], cells[l + 1][0], cells[l - 1][1], cells[l][1], cells[l + 1][1],]
+            return [this.cells[l - 1][0], this.cells[l + 1][0], this.cells[l - 1][1], this.cells[l][1], this.cells[l + 1][1],]
         }
         if (c === columns - 1) {
-            return [cells[l - 1][c], cells[l + 1][c], cells[l - 1][c - 1], cells[l][c - 1], cells[l + 1][c - 1],]
+            return [this.cells[l - 1][c], this.cells[l + 1][c], this.cells[l - 1][c - 1], this.cells[l][c - 1], this.cells[l + 1][c - 1],]
         }
-        return [cells[l - 1][c - 1], cells[l - 1][c], cells[l - 1][c + 1], cells[l][c - 1], cells[l][c + 1], cells[l + 1][c - 1], cells[l + 1][c], cells[l + 1][c + 1]]
+        return [this.cells[l - 1][c - 1], this.cells[l - 1][c], this.cells[l - 1][c + 1], this.cells[l][c - 1], this.cells[l][c + 1], this.cells[l + 1][c - 1], this.cells[l + 1][c], this.cells[l + 1][c + 1]]
     }
 }
